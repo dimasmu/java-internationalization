fbec5c2b7a4af2f2bce1d7b78255cf00e8e2c360
AzureAD\DimasMaendraUtomo <dimas.utomo@anteraja.id>
2025-01-06 10:07:14 +0700

    new project java internationalization

---
+++ src/main/java/com/example/java/internationalization/JavaInternationalizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.java.internationalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInternationalizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInternationalizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java/internationalization/JavaInternationalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java.internationalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInternationalizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
